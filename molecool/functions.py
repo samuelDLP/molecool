ab5d41c2827ab1e1978241fbbd1722197231ee1a
Samuel De La Paz <sdelapaz@nd.edu>
2020-12-17 09:35:54 -0500

    arrange functions into modules with io dir

--- molecool/functions.py
+++ molecool/functions.py
@@ -36,43 +36,6 @@ def canvas(with_attribution=True):
     return quote
 
 
-def open_pdb(f_loc):
-    # This function reads in a pdb file and returns the atom names and coordinates.
-    with open(f_loc) as f:
-        data = f.readlines()
-    c = []
-    sym = []
-    for l in data:
-        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
-            sym.append(l[76:79].strip())
-            c2 = [float(x) for x in l[30:55].split()]
-            c.append(c2)
-    coords = np.array(c)
-    return sym, coords
-
-
-def open_xyz(file_location):
-
-    # Open an xyz file and return symbols and coordinates.
-    xyz_file = np.genfromtxt(fname=file_location, skip_header=2, dtype='unicode')
-    symbols = xyz_file[:,0]
-    coords = (xyz_file[:,1:])
-    coords = coords.astype(np.float)
-    return symbols, coords
-
-def write_xyz(file_location, symbols, coordinates):
-
-    # Write an xyz file given a file location, symbols, and coordinates.
-    num_atoms = len(symbols)
-
-    with open(file_location, 'w+') as f:
-        f.write('{}\n'.format(num_atoms))
-        f.write('XYZ file\n')
-
-        for i in range(num_atoms):
-            f.write('{}\t{}\t{}\t{}\n'.format(symbols[i],
-                                              coordinates[i,0], coordinates[i,1], coordinates[i,2]))
-
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
     print(canvas())
