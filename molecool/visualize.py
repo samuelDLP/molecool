cbbce853a70e3eeb6f15a00223b2ddcd8fce8337
Samuel De La Paz <sdelapaz@nd.edu>
2021-01-04 14:48:46 -0500

    run black on modules

--- molecool/visualize.py
+++ molecool/visualize.py
@@ -5,10 +5,11 @@ Visualization
 import matplotlib as plt
 import numpy as np
 
-from mpl_toolkits.mplot3d import Axes3D # noqa: 4801
+from mpl_toolkits.mplot3d import Axes3D  # noqa: 4801
 
 # local import (from current directory), assumes python
 from .atom_data import atom_colors
+
 
 def draw_molecule(coordinates, symbols, draw_bonds=None, save_location=None, dpi=300):
 
@@ -16,17 +17,25 @@ def draw_molecule(coordinates, symbols, draw_bonds=None, save_location=None, dpi
 
     # Create figure
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(111, projection="3d")
 
     # Get colors - based on atom name
     colors = []
     for atom in symbols:
         colors.append(atom_colors[atom])
 
-    size = np.array(plt.rcParams['lines.markersize'] ** 2)*200/(len(coordinates))
+    size = np.array(plt.rcParams["lines.markersize"] ** 2) * 200 / (len(coordinates))
 
-    ax.scatter(coordinates[:,0], coordinates[:,1], coordinates[:,2], marker="o",
-               edgecolors='k', facecolors=colors, alpha=1, s=size)
+    ax.scatter(
+        coordinates[:, 0],
+        coordinates[:, 1],
+        coordinates[:, 2],
+        marker="o",
+        edgecolors="k",
+        facecolors=colors,
+        alpha=1,
+        s=size,
+    )
 
     # Draw bonds
     if draw_bonds:
@@ -34,8 +43,12 @@ def draw_molecule(coordinates, symbols, draw_bonds=None, save_location=None, dpi
             atom1 = atoms[0]
             atom2 = atoms[1]
 
-            ax.plot(coordinates[[atom1,atom2], 0], coordinates[[atom1,atom2], 1],
-                    coordinates[[atom1,atom2], 2], color='k')
+            ax.plot(
+                coordinates[[atom1, atom2], 0],
+                coordinates[[atom1, atom2], 1],
+                coordinates[[atom1, atom2], 2],
+                color="k",
+            )
 
     # Save figure
     if save_location:
@@ -43,9 +56,9 @@ def draw_molecule(coordinates, symbols, draw_bonds=None, save_location=None, dpi
 
     return ax
 
+
 def bond_histogram(bond_list, save_location=None, dpi=300, graph_min=0, graph_max=2):
     # Draw a histogram of bond lengths based on a bond_list (output from build_bond_list function)
-
 
     lengths = []
     for atoms, bond_length in bond_list.items():
@@ -56,9 +69,8 @@ def bond_histogram(bond_list, save_location=None, dpi=300, graph_min=0, graph_ma
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    plt.xlabel('Bond Length (angstrom)')
-    plt.ylabel('Number of Bonds')
-
+    plt.xlabel("Bond Length (angstrom)")
+    plt.ylabel("Number of Bonds")
 
     ax.hist(lengths, bins=bins)
 
